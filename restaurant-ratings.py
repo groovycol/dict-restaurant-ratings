960b3a50b950a5b791e097515ce704c0928004b6
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 17:45:46 -0700

    Receives user input and adds to dict.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,20 +1,28 @@
 def create_dict(filename):
-    """Return a dictionary and rating score."""
+    """Return a dictionary of restaurants and rating score."""
 
     restaurant_ratings = {}
 
-        #open file and make readable
-    working_file = open(filename)
+    with open(filename) as working_file:
 
-    for line in working_file:
-        line = line.rstrip()
-        data_in_line = line.split(':')
+        for line in working_file:
+            line = line.rstrip()
+            data_in_line = line.split(':')
 
-        restaurant_ratings[data_in_line[0]] = data_in_line[1]
-
-    working_file.close()
+            restaurant_ratings[data_in_line[0]] = data_in_line[1] 
 
     return restaurant_ratings
+
+
+def add_user_ratings(restaurant_info):
+    """Add new key value pair to the dictionary passed in. Return updated dictionary."""
+
+    user_rest = raw_input("Please enter a restaurant name: ")
+    user_rating = int(raw_input("Please enter a rating for {}: ".format (user_rest)))
+
+    restaurant_info[user_rest] = user_rating
+
+    return restaurant_info
 
 
 def print_sorted_data(restaurant_info):
@@ -24,4 +32,7 @@ def print_sorted_data(restaurant_info):
         print "{} is rated at {}.".format(key, restaurant_info[key])  
 
 
-print_sorted_data(create_dict('scores.txt'))
+
+restaurant_dictionary = create_dict('scores.txt')
+
+print_sorted_data(add_user_ratings(restaurant_dictionary))
